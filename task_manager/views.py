8f18f401f2867e60e9ef9d8d341b83a8b2852f54
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-01-14 20:54:12 +0200

    add index view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Task, TaskType, Position, Worker
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    uncompleted_tasks = Task.objects.filter(is_completed=False).count()
+    workers = Worker.objects.count()
+    positions = Position.objects.count()
+    task_types = TaskType.objects.count()
+    context = {
+        "completed_tasks": completed_tasks,
+        "uncompleted_tasks": uncompleted_tasks,
+        "workers": workers,
+        "positions": positions,
+        "task_types": task_types,
+    }
+    return render(request, "index.html", context)
