5a338143b9a734692ec334335cde8c9e0f22d251
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-01-16 16:14:19 +0200

    add forms and views for Task model
    update and delete

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Message
+from task_manager.models import Message, Worker, Task
 
 
 class MessageForm(forms.ModelForm):
@@ -11,3 +12,45 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ["text"]
+
+
+class WorkerCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "position",
+        )
+
+
+class WorkerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "position",
+        )
+
+
+class TaskCreateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "assignees": forms.CheckboxSelectMultiple()
+        }
+
+
+class TaskUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "assignees": forms.CheckboxSelectMultiple(),
+            "tags": forms.CheckboxSelectMultiple()
+        }
